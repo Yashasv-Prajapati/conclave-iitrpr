54e6d8d1368fcc2d442e8f68aa153d3ddb86bfae
Yashasav Prajapati <40720591+Yashasv-Prajapati@users.noreply.github.com>
2022-10-10 04:13:10 +0530

    Add files via upload

--- script/nav.js
+++ script/nav.js
@@ -20,3 +20,36 @@ function navHighlighter() {
     }
   });
 }
+
+const btn = document.getElementById("read-more");
+
+let hidden = true;
+
+btn.addEventListener("click", ReadMore);
+
+function InitializeButton(){
+  hidden = true;
+  document.getElementById("read-more-content").style.display = "none";
+  document.getElementById("prelude-content").style.fontSize = "1rem";
+}
+
+
+function ReadMore(){
+  
+  const content = document.getElementById("read-more-content");
+
+    if(hidden){
+        content.style.display = "flex";
+        btn.innerText = "Read Less";
+        document.getElementById("prelude-content").style.fontSize = "0.8rem";
+        hidden = false;
+      }else{
+        content.style.display = "none";
+        document.getElementById("prelude-content").style.fontSize = "1rem";
+        btn.innerText = "Read More";
+        hidden = true;
+    }
+
+}
+
+window.onload = InitializeButton;
